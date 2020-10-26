2b1fbd96f47008720e7e781608334e24b66b1d7e
RomaLevkiv <kvintaslev@i.ua>
2020-10-26 09:50:47 +0200

    auth-part1-finish

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common"
+import { AuthGuard } from "@nestjs/passport"
+import { CreateTaskDto } from "./dto/create-task.dto"
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto"
+import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe"
+import { TaskStatus } from "./task-status.enum"
+import { Task } from "./task.entity"
+import { TasksService } from "./tasks.service"
+
+@Controller("tasks")
+@UseGuards(AuthGuard())
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+    return this.tasksService.getTasks(filterDto)
+  }
+
+  @Get(":id")
+  getTaskById(@Param("id", ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id)
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto)
+  }
+
+  @Delete(":id")
+  deleteTask(@Param("id", ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteById(id)
+  }
+
+  @Patch(":id/status")
+  patchStatusById(
+    @Param("id", ParseIntPipe) id: number,
+    @Body("status", TaskStatusValidationPipe) status: TaskStatus
+  ): Promise<Task> {
+    return this.tasksService.patchStatusById(id, status)
+  }
+}
